698a5358a97ea97125de1939a0ff986af84ddeb9
Wei Zhang <kweizh@tabbyml.com>
2026-06-07 14:25:04 +0000

    fix: clean up debug jobs

--- jobs/2026-06-06__15-35-54/liveblocks-cursor-positions__LY8npzh/artifacts/user/myproject/app/layout.tsx
+++
@@ -1,9 +0,0 @@
-export const metadata = { title: "Liveblocks Cursors" };
-
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <body style={{ margin: 0 }}>{children}</body>
-    </html>
-  );
-}
